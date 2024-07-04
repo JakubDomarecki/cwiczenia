6dc763822aea7c3cfa071de9323c3b086c59ba09
JakubDomarecki <kuba86952@wp.pl>
2024-07-04 17:28:30 +0200

    natywna funkcja

--- Natywna metoda/natywnaMetoda/src/js.js
+++ Natywna metoda/natywnaMetoda/src/js.js
@@ -1,4 +1,4 @@
 String.prototype.setAtTheEnd = function (word) {
-    return this.replace(word, "") + " " + word;
+    return `${this.replace(word,"")} ${word}`;
 }
 console.log("Ala ma kota Bzika".setAtTheEnd("kota"));
